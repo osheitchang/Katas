4a1c57867c0e7edc7c1f54ef2cad399940853e73
Osheit <chang.osheit@gmail.com>
2019-09-19 10:27:08 -0400

    corrections to last version

--- starter-code.js
+++ starter-code.js
@@ -1,12 +1,7 @@
 function wave(str){
-    let letter;
-     let x =-1;
-      for(i=0;i<str.length;i++){
-    if(str[i] !="" || str !== "") {
-    var newArr = str.split("").map(char=>{return str.split("").fill(char.toUpperCase(),x+=1,x+1).join("");})
-    }
-    else{console.log("hello")}
-    return newArr
-    }
-   }
-  wave('')
+  let x =-1;
+ var newArr = str.split("").map(char=> {return str.split("").fill(char.toUpperCase(),x+=1,x+1).join("")
+ })
+ let result = newArr.filter(word=>{ return word !== word.toLowerCase()})
+ return result
+  }
